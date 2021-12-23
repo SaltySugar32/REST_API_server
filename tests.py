cb49ff10abad57db52fe1f84db8423c381c16142
Vitaly <45869757+SaltySugar32@users.noreply.github.com>
2021-12-23 11:00:18 +1000

    [#9] added tests file

---
+++ tests.py
@@ -0,0 +1,18 @@
+""" Test for CI/CD pipeline """
+import unittest
+from main import app as rest_api
+
+
+class TestFlaskApi(unittest.TestCase):
+    def setUp(self):
+        self.app = rest_api.test_client()
+
+    def test_home(self):
+        response = self.app.get("/")
+        print(response.json)
+        self.assertEqual(response.json, "SaltySugar REST_API_server")
+
+
+if __name__ == "__main__":
+    with rest_api.app_context():
+        unittest.main()
